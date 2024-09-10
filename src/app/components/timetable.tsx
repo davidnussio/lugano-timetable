38e61bb9e3d5158614918aee6a3fbbfe0d689ff8
David Nussio <david.nussio@gmail.com>
2024-09-10 20:53:42 +0200

    fix: timetable images

--- src/app/components/timetable.tsx
+++ src/app/components/timetable.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { SquareChartGantt } from "lucide-react";
+import Image from "next/image";
 import { Button } from "~/components/ui/button";
 import {
   Dialog,
@@ -20,19 +21,20 @@ export function Timetable({ url }: { url: string }) {
           <SquareChartGantt size={18} />
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-full max-h-full h-full">
+      <DialogContent className="max-w-full max-h-full h-full flex flex-col">
         <DialogHeader>
           <DialogTitle>Oriario</DialogTitle>
           <DialogDescription>
             Consulta gli orari aggiornati per la fermata del bus.
           </DialogDescription>
         </DialogHeader>
-        <div className="mx-auto">
-          <img
+        <div className="m-auto h-full w-full relative flex-grow justify-end">
+          <Image
             alt="Timetable"
             title="Image"
             src={url}
-            // className="w-full h-full"
+            fill
+            style={{ objectFit: "contain", margin: "4rem" }}
           />
         </div>
         <DialogFooter>
